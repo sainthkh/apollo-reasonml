3ea47732a6c19dc4d52ff7c976aa2be41e581ca3
sainthkh <sainthkh@naver.com>
2019-02-25 17:34:21 +0900

    Fixed the path to the compiler package.

--- scripts/apollo-reasonml-compiler.cmd
+++ scripts/apollo-reasonml-compiler.cmd
@@ -1,7 +1,7 @@
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\..\..\..\packages\apollo-reasonml-compiler\src\cli.js" %*
+  "%~dp0\node.exe"  "%~dp0\..\..\..\..\..\packages\compiler\src\cli.js" %*
 ) ELSE (
   @SETLOCAL
   @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\..\..\..\packages\apollo-reasonml-compiler\src\cli.js" %*
+  node  "%~dp0\..\..\..\..\..\packages\compiler\src\cli.js" %*
 )
